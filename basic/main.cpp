56166ebc6619e8d3ed89356edc975af4d59c8ea6
ram kumar <rr697144@gmail.com>
2025-08-09 17:00:33 +0530

    difference of two number

---
+++ basic/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Enter two number : " << endl;
+
+    int number1, number2;
+
+    cin >> number1 >> number2;
+
+    int difference = number1 - number2;
+
+    cout << difference << endl;
+
+    return 0;
+}
