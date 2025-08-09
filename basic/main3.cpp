0075fc9c0ac9cfd1fcb5283379e42daf958daed3
ram kumar <rr697144@gmail.com>
2025-08-09 17:27:55 +0530

    average of two number

---
+++ basic/main3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Enter two number : " << endl;
+
+    int number1, number2;
+
+    cin >> number1 >> number2;
+
+    int avg = (number1 + number2) / 2;
+
+    cout << "average of two number is " << avg << endl;
+
+    return 0;
+}
