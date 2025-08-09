7de26d2cbf9ca018ee80734a947c3c12580cea03
ram kumar <rr697144@gmail.com>
2025-08-09 17:04:16 +0530

    multiplication of two number

---
+++ basic/main2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Enter two number : " << endl;
+
+    int number1, number2;
+
+    cin >> number1 >> number2;
+
+    int product = number1 * number2;
+
+    cout << "multiplication of two number is " << product << endl;
+
+    return 0;
+}
