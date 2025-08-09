bb7b3e2a1a08388157eedc969f42cad0946bf36a
ram kumar <rr697144@gmail.com>
2025-08-09 17:02:19 +0530

    sum of two number

---
+++ basic/main1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Enter two number : " << endl;
+
+    int number1, number2;
+
+    cin >> number1 >> number2;
+
+    int sum = number1 + number2;
+
+    cout << "sum of two number is " << sum << endl;
+
+    return 0;
+}
